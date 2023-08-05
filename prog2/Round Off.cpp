32e02b8aaa650815d5c549f617a34670290e9f1a
KRISHNA PRABHA S <85541860+krishnaprabha21@users.noreply.github.com>
2023-08-05 14:28:46 +0530

    Create Round Off.cpp

---
+++ prog2/Round Off.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    float a;
+    cin>>a;
+    cout<<(int)a<<endl<<ceil(a)<<endl<<floor(a);
+    return 0;
+}
