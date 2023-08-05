59cafe2919cd37740fa011e80aacb5f40ba68960
KRISHNA PRABHA S <85541860+krishnaprabha21@users.noreply.github.com>
2023-08-05 14:37:11 +0530

    The Newspaper Agency.cpp

---
+++ prog4/The Newspaper Agency.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int a,b,c;
+    cin>>a>>b>>c;
+    int p=(a*(b-c)-100);
+    cout<<p;
+    return 0;
+}
