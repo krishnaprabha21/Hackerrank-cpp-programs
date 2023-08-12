4ef5eb15c65f3f36994de507fcd47a24df2bd6c4
KRISHNA PRABHA S <85541860+krishnaprabha21@users.noreply.github.com>
2023-08-12 10:54:55 +0530

    transpose array 2D.cpp

---
+++ prog44/transpose array 2D.cpp
@@ -0,0 +1,43 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n;
+    cin>>n;
+    int a[n][n];
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+             cin>>a[i][j];
+        }
+    }
+       
+    
+     for(int i=0;i<n;i++)
+    {
+         for(int j=0;j<n;j++)
+        {
+             cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    
+    cout<<"Transpose matrix is:"<<endl;
+    
+    for(int i=0;i<n;i++)
+    {
+         for(int j=0;j<n;j++)
+        {
+             cout<<a[j][i]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
